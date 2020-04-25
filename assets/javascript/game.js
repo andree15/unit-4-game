365097f782ec25138e66972f6fd48392987b3183
Andy Edstrom <andyedstrom115@gmail.com>
2020-04-25 17:10:43 -0400

    Added and completed html and CSS files

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//set a variable that displayes a random number between 19-120
+//give the gems a random number generator from 1-12
+//display the random number when clicking the gems
+    //variable where a random number is generated when clicking
+//the player loses when they go over the random given number
+    //add one to "losses"
+//the player wins when their total score matches the random number
+
